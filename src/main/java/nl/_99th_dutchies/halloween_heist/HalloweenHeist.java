f9af8a60ab7de34d7879291144734babefb10c4b
Jordy van der Poel <jordyvanderpoel@gmail.com>
2021-09-16 22:35:06 +0200

    Setup base of plugin

---
+++ src/main/java/nl/_99th_dutchies/halloween_heist/HalloweenHeist.java
@@ -0,0 +1,15 @@
+package nl._99th_dutchies.halloween_heist;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class HalloweenHeist extends JavaPlugin {
+    @Override
+    public void onEnable(){
+        //Fired when the server enables the plugin
+    }
+
+    @Override
+    public void onDisable(){
+        //Fired when the server stops and disables all plugins
+    }
+}
