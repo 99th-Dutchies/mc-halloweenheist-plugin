d3d76b8126d35ed65ec220281e36aeebf4363b13
Jordy van der Poel <jordyvanderpoel@gmail.com>
2021-09-18 20:53:43 +0200

    Add items to command /kit

--- src/main/java/nl/_99th_dutchies/halloween_heist/command/CommandKit.java
+++ src/main/java/nl/_99th_dutchies/halloween_heist/command/CommandKit.java
@@ -8,7 +8,10 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.metadata.FixedMetadataValue;
+
+import java.util.ArrayList;
 
 public class CommandKit implements CommandExecutor {
     HalloweenHeist plugin = HalloweenHeist.getPlugin(HalloweenHeist.class);
@@ -25,11 +28,58 @@ public class CommandKit implements CommandExecutor {
         if (p.hasMetadata("nl._99th_dutchies.halloween_heist.hasUsedKit")) {
             sender.sendMessage(ChatColor.RED + "You already obtained your kit");
         } else {
-            p.getInventory().addItem(new ItemStack(Material.COOKIE, 1));
+            ArrayList<InventoryItem> items = new ArrayList<InventoryItem>();
+            items.add(new InventoryItem(Material.CHAINMAIL_BOOTS, 1, "Tactical gear"));
+            items.add(new InventoryItem(Material.CHAINMAIL_CHESTPLATE, 1, "Tactical gear"));
+            items.add(new InventoryItem(Material.CHAINMAIL_HELMET, 1, "Tactical gear"));
+            items.add(new InventoryItem(Material.CHAINMAIL_LEGGINGS, 1, "Tactical gear"));
+            items.add(new InventoryItem(Material.IRON_SWORD, 1, "Zorton's Sword"));
+            items.add(new InventoryItem(Material.IRON_PICKAXE, 1));
+            items.add(new InventoryItem(Material.IRON_AXE, 1, "Rosa's Battle Axe"));
+            items.add(new InventoryItem(Material.IRON_SHOVEL, 1));
+            items.add(new InventoryItem(Material.IRON_HOE, 1));
+            items.add(new InventoryItem(Material.TORCH, 16));
+            items.add(new InventoryItem(Material.GOLDEN_APPLE, 1, "Breakfast burrito"));
+            items.add(new InventoryItem(Material.COOKED_CHICKEN, 8, "Wing Slut Chicken"));
+            items.add(new InventoryItem(Material.COOKED_BEEF, 8, "Steak Buds Steak"));
+            items.add(new InventoryItem(Material.MUSHROOM_STEW, 8, "Terry's Yoghurt"));
+            items.add(new InventoryItem(Material.COOKIE, 8, "Marshed Mallow"));
+            items.add(new InventoryItem(Material.BREAD, 8, "Mother Dough Bread"));
+            items.add(new InventoryItem(Material.OAK_LOG, 16));
+
+            for(InventoryItem item : items) {
+                ItemStack is = new ItemStack(item.material, item.count);
+
+                if(item.name != null) {
+                    ItemMeta ism = is.getItemMeta();
+                    ism.setDisplayName(item.name);
+                    is.setItemMeta(ism);
+                }
+
+                p.getInventory().addItem(is);
+            }
 
             p.setMetadata("nl._99th_dutchies.halloween_heist.hasUsedKit", new FixedMetadataValue(plugin, true));
         }
 
         return true;
+    }
+}
+
+class InventoryItem {
+    public final Material material;
+    public final int count;
+    public final String name;
+
+    InventoryItem(Material material, int count) {
+        this.material = material;
+        this.count = count;
+        this.name = null;
+    }
+
+    InventoryItem(Material material, int count, String name) {
+        this.material = material;
+        this.count = count;
+        this.name = name;
     }
 }
