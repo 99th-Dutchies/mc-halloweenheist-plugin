c045e68cf426d1634d7be703bbbf38db4115922a
Jordy van der Poel <jordyvanderpoel@gmail.com>
2021-10-04 19:27:37 +0200

    Add scoreboard (implements #15)

--- src/main/java/nl/_99th_dutchies/halloween_heist/HalloweenHeistPlugin.java
+++ src/main/java/nl/_99th_dutchies/halloween_heist/HalloweenHeistPlugin.java
@@ -4,20 +4,23 @@ import nl._99th_dutchies.halloween_heist.command.CommandKit;
 import nl._99th_dutchies.halloween_heist.command.CommandTrade;
 import nl._99th_dutchies.halloween_heist.listener.HeistObjectSavingListener;
 import nl._99th_dutchies.halloween_heist.listener.HeistObjectTrackingListener;
+import nl._99th_dutchies.halloween_heist.scoreboard.ScoreboardManager;
 import nl._99th_dutchies.halloween_heist.season.ISeason;
 import nl._99th_dutchies.halloween_heist.season.Season1;
-import nl._99th_dutchies.halloween_heist.util.HeistObjectLocation;
-import nl._99th_dutchies.halloween_heist.util.LocationBroadcaster;
-import nl._99th_dutchies.halloween_heist.util.RealTimeCycle;
-import nl._99th_dutchies.halloween_heist.util.WinnerAnnouncer;
+import nl._99th_dutchies.halloween_heist.util.*;
 import org.bukkit.*;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.*;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class HalloweenHeistPlugin extends JavaPlugin implements Listener {
+    private static ScoreboardManager scoreboardManager;
+
     public final FileConfiguration config = getConfig();
     public HeistObjectLocation heistObjectLocation;
     public int lastLocationBroadcastHour = -1;
@@ -33,6 +36,8 @@ public class HalloweenHeistPlugin extends JavaPlugin implements Listener {
         this.mainWorld = this.getServer().getWorld(this.config.getString("worldName"));
         this.setSeason();
         this.heistObjectLocation = new HeistObjectLocation(this.mainWorld, null,null);
+        this.scoreboardManager = new ScoreboardManager();
+        this.startUpdateTask();
 
         Bukkit.getPluginManager().registerEvents(this, this);
         Bukkit.getPluginManager().registerEvents(new HeistObjectTrackingListener(this), this);
@@ -62,9 +67,29 @@ public class HalloweenHeistPlugin extends JavaPlugin implements Listener {
         }
     }
 
+    private void startUpdateTask() {
+        Bukkit.getScheduler().runTaskTimer(this, () -> {
+            scoreboardManager.getPlayerScoreboards().values().forEach((scoreboard) -> scoreboard.update());
+        }, 0L, 1L); // Very fast, every tick.
+    }
+
     @EventHandler
     public void onPlayerDeath(PlayerDeathEvent event) {
         event.getEntity().removeMetadata("nl._99th_dutchies.halloween_heist.hasUsedKit", this);
         this.heistObjectLocation.find(this.season.getHeistObjectMaterial());
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e) {
+        Player p = e.getPlayer();
+
+        scoreboardManager.addToPlayerCache(p);
+    }
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent e) {
+        Player p = e.getPlayer();
+
+        scoreboardManager.removeFromPlayerCache(p);
     }
 }
---
+++ src/main/java/nl/_99th_dutchies/halloween_heist/scoreboard/ScoreboardManager.java
@@ -0,0 +1,47 @@
+package nl._99th_dutchies.halloween_heist.scoreboard;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+
+import de.beproud.scoreboard.PlayerScoreboard;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ScoreboardManager implements Listener {
+
+    private ConcurrentHashMap<Player, PlayerScoreboard> playerScoreboards;
+
+    public ScoreboardManager() {
+        this.playerScoreboards = new ConcurrentHashMap<>();
+
+        Bukkit.getOnlinePlayers().forEach((player) -> this.addToPlayerCache(player));
+    }
+
+    public boolean hasCachedPlayerScoreboard(Player p) {
+        return this.playerScoreboards.containsKey(p);
+    }
+
+    public void addToPlayerCache(Player p) {
+        if (!this.hasCachedPlayerScoreboard(p))	{
+            this.playerScoreboards.put(p, new PlayerScoreboard(new DefaultProvider(), p));
+        }
+    }
+
+    public void removeFromPlayerCache(Player p) {
+        if (this.hasCachedPlayerScoreboard(p)) {
+            this.playerScoreboards.remove(p).disappear();
+        }
+    }
+
+    public PlayerScoreboard getPlayerScoreboard(Player p) {
+        if (!this.hasCachedPlayerScoreboard(p)) this.addToPlayerCache(p);
+
+        return this.playerScoreboards.get(p);
+    }
+
+    public ConcurrentHashMap<Player, PlayerScoreboard> getPlayerScoreboards() {
+        return this.playerScoreboards;
+    }
+
+}
